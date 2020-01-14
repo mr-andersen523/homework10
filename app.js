30e5377221311ef68d7279b9030bc591a1301615
mr andersen <chasea523@gmail.com>
2020-01-14 11:31:04 -0800

    start of function

--- app.js
+++ app.js
@@ -7,4 +7,32 @@ const Manager = require("./lib/Manager");
 const Intern = require("./lib/Intern");
 const Engineer = require("./lib/Engineer");
 
+////////////////////////////////////////////////
 
+async function start(){
+    console.log("Create your Engineering Team!");
+
+    // variable to hold HTML
+    let teamHTML = "";
+
+    // 
+    let teamSize;
+
+    // 
+    await inquirer.prompt(
+        {
+            type: "number",
+            message: "How many people are on your Engineering team?",
+            name: "numOfTeam"
+        }
+    )
+    .then((data) => {
+
+        teamSize = data.numOfTeam + 1;
+    });
+    
+    // If Team Size is 0, will end program
+    if (teamSize === 0){
+        console.log("You can't make a team without team members...");
+        return;
+    }
