4d3846fd43a703ac54fb716cdce2aab6938f677a
mr andersen <chasea523@gmail.com>
2020-01-14 10:53:44 -0800

    added packages and require

---
+++ app.js
@@ -0,0 +1,10 @@
+//node packages
+
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const Manager = require("./lib/Manager");
+const Intern = require("./lib/Intern");
+const Engineer = require("./lib/Engineer");
+
+
